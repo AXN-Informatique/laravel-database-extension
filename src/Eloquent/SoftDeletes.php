134868bd2ae32ba4554e4690aefeaf5095004bf7
Lucas GIRARDOT <lucas.girardot@axn.fr>
2021-10-03 20:23:44 +0200

    tweak

--- src/Eloquent/SoftDeletes.php
+++ src/Eloquent/SoftDeletes.php
@@ -2,31 +2,40 @@
 
 namespace Axn\Illuminate\Database\Eloquent;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletes as EloquentSoftDeletes;
 
 trait SoftDeletes
 {
     use EloquentSoftDeletes;
 
-    public function scopeWithoutTrashedExcept($query, $exceptId = null)
+    /**
+     * Like "withoutTrashed()" but with the ability of excepting some records
+     * (these records will be retrieved even if they are trashed).
+     *
+     * @param  Builder        $query
+     * @param  int|array[int] $exceptId
+     * @return void
+     */
+    public function scopeWithoutTrashedExcept(Builder $query, $exceptId = null)
     {
-        // Replace :
-        //  $query->where(function($query) use ($exceptId){
-        //
+        // Replaced :
+        //   $query->where(function ($query) use ($exceptId) {
         // By :
-        //  $query->where(function() use ($query, $exceptId){
+        //   $query->where(function () use ($query, $exceptId) {
         //
-        // If we do not do that the builder loses his scopes...
+        // If we do not do that, the builder loses his scopes...
 
-        $query->where(function() use ($query, $exceptId){
-            $query->withoutTrashed();
-            $query->when($exceptId, function($query, $exceptId){
-                if (is_array($exceptId)) {
-                    $query->orWhereIn($this->getQualifiedKeyName(), $exceptId);
-                } else {
-                    $query->orWhere($this->getQualifiedKeyName(), $exceptId);
-                }
-            });
+        $query->where(function () use ($query, $exceptId) {
+            $query
+                ->withoutTrashed()
+                ->when($exceptId, function ($query, $exceptId) {
+                    if (is_array($exceptId)) {
+                        $query->orWhereIn($this->getQualifiedKeyName(), $exceptId);
+                    } else {
+                        $query->orWhere($this->getQualifiedKeyName(), $exceptId);
+                    }
+                });
         });
     }
 }
