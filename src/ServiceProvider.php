83fded83cbedc11a0ae52565523e1f2cd4656d61
Lucas GIRARDOT <lucas.girardot@axn.fr>
2021-10-03 20:22:33 +0200

    Remove extension of MySqlConnection

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,33 +2,12 @@
 
 namespace Axn\Illuminate\Database;
 
-use Illuminate\Database\Connection;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
 class ServiceProvider extends BaseServiceProvider
 {
-    public function register()
-    {
-        $this->replaceMySqlConnection();
-    }
-
     public function boot()
     {
         require __DIR__.'/macros.php';
-    }
-
-    /**
-     * Replace MySqlConnection in the IoC by the extended one.
-     *
-     * @return void
-     */
-    protected function replaceMySqlConnection()
-    {
-        Connection::resolverFor(
-            'mysql',
-            function($connection, $database, $prefix, $config) {
-                return new MySqlConnection($connection, $database, $prefix, $config);
-            }
-        );
     }
 }
